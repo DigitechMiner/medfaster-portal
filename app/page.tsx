d61fd54796d6b55d242dddd8908dbbf3970a9677
SPaCE69 <rahulnishad6969@gmail.com>
2025-10-24 21:17:02 +0530

    Header added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@/components/ui/button";
-
+import Header from "@/components/header"
 export default function Home() {
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <Header />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
 
         <div className="flex gap-3">
